43ec0c9b64846573af424048e006efae60eee4d6
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2022-11-16 12:50:54 +0100

    IT255-DZ07

---
+++ src/app/o-nama/o-nama.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-o-nama',
+  templateUrl: './o-nama.component.html',
+  styleUrls: ['./o-nama.component.scss']
+})
+export class ONamaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
