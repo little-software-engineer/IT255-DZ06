bc7f96bb2219bde3caa253bb384383754c330bf7
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2022-11-29 15:02:03 +0100

    IT255-DZ09

--- src/app/available-apartments/available-apartments.component.ts
+++ src/app/available-apartments/available-apartments.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { RoomServiceService } from 'src/app/services/room-service.service';
+import { Room } from '../modules/Room';
 
 @Component({
   selector: 'app-available-apartments',
@@ -7,11 +9,17 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class AvailableApartmentsComponent implements OnInit {
 
+  @Input() room: Room;
+
+  [x: string]: any;
+  public pricesSum: any;
+  public apartmani: any;
   public availableApartments = [
     {
       room: 1,
       floor: 1,
-      price: 1000,
+      price: 600,
+      nights: 5,
       desc: 'Hotel Waldorf Astoria u centru Berlina' +
         ' je jedan od najlepsih hotela sa 5 zvezdica ' +
         'u ovom delu Evrope' +
@@ -22,6 +30,7 @@ export class AvailableApartmentsComponent implements OnInit {
       room: 2,
       floor: 1,
       price: 1000,
+      nights: 3,
       desc: 'Hotel Hard Rock Moskva' +
         ' je jos jedan u nizu tematskih hotela '  +
         'ove poznate fransize tematskih restorana' +
@@ -32,7 +41,10 @@ export class AvailableApartmentsComponent implements OnInit {
 
   @Input() newApartment: any;
 
-  constructor() {
+
+  constructor(private room_service: RoomServiceService) {
+    this.room = new Room(3, 3, 600, 5, "Hotel u centru Beogarda zraci svojim luksuzom")
+
   }
 
   ngOnInit(): void {
@@ -40,4 +52,9 @@ export class AvailableApartmentsComponent implements OnInit {
       this.availableApartments.push(this.newApartment);
     }
   }
+
+  public price() {
+    this.pricesSum = this.room.nights * this.room.price;
+    return this.room_service.getPrice(this.pricesSum);
+  };
 }
