7ad30a2d83e9294f0d5b4b69361926a5176c9554
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2022-12-07 20:25:33 +0100

    IT255-DZ10

--- src/app/models/room.model.ts
+++ src/app/models/room.model.ts
@@ -1,1 +1,16 @@
-
+export class room{
+    id: number;
+    room: number;
+    floor:number;
+    price: number;
+    nights: number;
+    desc: string;
+    constructor(id: number, room: number, floor: number, price: number, nights: number, desc: string){
+        this.id = id;
+        this.room = room;
+        this.floor = floor;
+        this.price = price;
+        this.nights = nights;
+        this.desc = desc;
+    }
+}
