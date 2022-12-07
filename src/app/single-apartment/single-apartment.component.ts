991e747483a5982afaec327ab5a3948b4b16bf07
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2022-12-07 20:24:21 +0100

    IT255-DZ10

--- src/app/single-apartment/single-apartment.component.ts
+++ src/app/single-apartment/single-apartment.component.ts
@@ -1,1 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { room } from 'src/app/models/room.model';
+import { DataService } from 'src/app/services/data.service';
 
+@Component({
+  selector: 'app-single-apartment',
+  templateUrl: './single-apartment.component.html'
+
+})
+export class SingleApartmentComponent implements OnInit {
+  id!: number;
+  room!: room;
+  loaded = false;
+  constructor(private route: ActivatedRoute, private dataService: DataService, private router: Router) {
+    route.params.subscribe(params => this.id = params['id']);
+  }
+
+  ngOnInit(): void {
+    this.dataService.getRoom(this.id).subscribe(data => {
+      this.room = data
+      this.loaded = true;
+    })
+  }
+
+  onDelete(){
+    this.dataService.delete(this.room).subscribe(() =>{
+      this.router.navigate(['/ponuda'])
+    })
+  }
+
+  onUpdate(){
+    this.router.navigate(['/editroom', this.room.id])
+  }
+
+}
