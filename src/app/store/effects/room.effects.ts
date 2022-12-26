fbf21ea1e76129bc7884af6a616a53bdd302d745
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2022-12-26 18:57:26 +0100

    IT255-DZ13

--- src/app/store/effects/room.effects.ts
+++ src/app/store/effects/room.effects.ts
@@ -1,1 +1,26 @@
+import { Injectable } from "@angular/core";
+import { DataService } from "src/app/services/data.service";
+import { Actions, createEffect, ofType } from "@ngrx/effects"
+import { EnumRoomAction, GetRooms, GetRoomsSuccess } from "../actions/room.action";
+import { of } from "rxjs";
+import { Room } from "src/app/interfaces/room";
+import { switchMap } from "rxjs/operators";
 
+
+@Injectable()
+export class RoomEffect {
+    constructor(private _actions$: Actions, private _roomService: DataService) {
+
+    }
+
+
+    getRooms$ = createEffect(() =>
+        this._actions$.pipe(
+            ofType<GetRooms>(EnumRoomAction.GetRooms),
+            switchMap(() => this._roomService.getRooms()),
+            switchMap((roomHttp: Room[]) => {
+                return of(new GetRoomsSuccess(roomHttp))
+            })
+        )
+    )
+}
