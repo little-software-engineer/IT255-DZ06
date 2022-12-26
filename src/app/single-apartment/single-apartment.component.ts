7735a213a47946bf3b7ecfcd57843b4ecede0d74
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2022-12-26 19:10:19 +0100

    IT255-DZ13

--- src/app/single-apartment/single-apartment.component.ts
+++ src/app/single-apartment/single-apartment.component.ts
@@ -1,8 +1,13 @@
-//pogled, odnosno view za individualnu sobu i njene detalje
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  EventEmitter, Input, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { room } from 'src/app/models/room.model';
 import { DataService } from 'src/app/services/data.service';
+import { selectedRooms, selectHoteli } from 'src/app/store/selector/post.selector';
+import { RoomState } from 'src/app/store/state/room.state';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Room } from 'src/app/interfaces/room';
+import { GetRooms } from '../store/actions/room.action';
 
 @Component({
   selector: 'app-single-apartment',
@@ -12,15 +17,22 @@ import { DataService } from 'src/app/services/data.service';
 export class SingleApartmentComponent implements OnInit {
   id!: number;
   room!: room;
+  public rooms$: Observable<Room[]>;
+  public rooms!: Room[];
   loaded = false;
-  constructor(private route: ActivatedRoute, private dataService: DataService, private router: Router) {
+  constructor(private route: ActivatedRoute, private dataService: DataService, private router: Router, private _store: Store<RoomState>,) {
+    this.rooms$ = this._store.pipe(select(selectedRooms));
+    this.rooms$.subscribe(val => {
+      this.rooms = JSON.parse(JSON.stringify(val));
     route.params.subscribe(params => this.id = params['id']);
-  }
+  })
+}
 
   ngOnInit(): void {
     this.dataService.getRoom(this.id).subscribe(data => {
       this.room = data
       this.loaded = true;
+      this._store.dispatch(new GetRooms());
     })
   }
 
